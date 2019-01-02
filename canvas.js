a3ddbe1d03a294bb6ed5d4845723abd711cce0fb
christocarr <christocarr@outlook.com>
2019-01-02 21:08:34 +0000

    Add random radii and color

--- canvas.js
+++ canvas.js
@@ -5,16 +5,18 @@ window.onload = function() {
 
   const c = CANVAS.getContext('2d');
 
-  function Circle(x, y, dx, dy, radius) {
+  function Circle(x, y, dx, dy, radius, color) {
     this.x = x;
     this.y = y;
     this.dx = dx;
     this.dy = dy;
     this.radius = radius;
+    this.color = color;
 
     this.draw = function() {
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+      c.strokeStyle = '#' + this.color;
       c.stroke();
     }
 
@@ -37,13 +39,15 @@ window.onload = function() {
   let circleArr = [];
 
   for (let i = 0; i < 50; i++) {
-    let radius = 20;
+    let radius = (Math.random() * 20) + 5;
     let x = Math.random() * (innerWidth - radius * 2) + radius; //starting point of circle on x-axis
     let y = Math.random() * (innerHeight - radius * 2) + radius; //starting poing of circle on y-axis
     let dx = (Math.random() - 0.5) * 8; //velocity on x-axis
     let dy = (Math.random() - 0.5) * 8; //velocity on y-axis
+    let color = Math.floor(Math.random()*16777215).toString(16);
 
-    circleArr.push(new Circle(x, y, dx, dy, radius));
+    circleArr.push(new Circle(x, y, dx, dy, radius, color));
+    console.log(circleArr);
   }
 
   function animate() {
